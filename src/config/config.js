f1b0d6cdb202d8d6fcfced833c9c9fc3c39cc6b8
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-16 16:28:11 +0700

    feat: add env configuration

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+    port: process.env.PORT,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExp: process.env.JWT_EXPIRES_IN,
+
+    rateLimit: {
+        globalWindow: Number(process.env.GLOBAL_RATE_LIMIT_WINDOW_MS),
+        globalMax: Number(process.env.GLOBAL_RATE_LIMIT_MAX),
+        authWindow: Number(process.env.AUTH_RATE_LIMIT_WINDOW_MS),
+        authMax: Number(process.env.AUTH_RATE_LIMIT_MAX),
+    },
+
+    logger: {
+        level: process.env.LOG_LEVEL,
+        dir: process.env.LOG_DIR,
+    },
+
+    nodeEnv: process.env.NODE_ENV
+}
