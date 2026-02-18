0b51449cd433ff1c90c28aa108a6fe8018ee09ce
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-18 16:31:12 +0700

    feat: fix check today endpoint

--- src/modules/track/track.controller.js
+++ src/modules/track/track.controller.js
@@ -4,15 +4,14 @@ import { checkHabitToday } from "./track.service.js";
 
 export const checkHabitController = async (req, res, next) => {
     try {
-        const habitId = req.params.id;
-
-        if (!habitId) {
+        const habitId = Number(req.params.id);
+        if (isNaN(habitId)) {
             throw new AppError({
-                message: "Habit ID is required",
+                message: "Invalid habit ID",
                 statusCode: 400,
                 code: "BAD_REQUEST"
             });
-        };
+        }
 
         await checkHabitToday(habitId, req.user.id);
 
