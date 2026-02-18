5cdae59cc75af3475c9777d4cedf4d12df04a664
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-18 16:48:59 +0700

    fix: add missing service imports

--- src/modules/track/track.controller.js
+++ src/modules/track/track.controller.js
@@ -1,6 +1,6 @@
 import { AppError } from "../../utils/AppError.js";
 import { success } from "../../utils/response.js";
-import { checkHabitToday } from "./track.service.js";
+import { checkHabitToday, getHabitStreak, getHabitStats} from "./track.service.js";
 
 export const checkHabitController = async (req, res, next) => {
     try {
