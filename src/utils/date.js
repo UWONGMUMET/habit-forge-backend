9e6206295e61d390912b9a4900ed7e89921172e5
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-18 15:52:29 +0700

    feat: add date helper utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,21 @@
 import dayjs from "dayjs";
 
-export const today = ()=> dayjs().startOf("day").toDate();
-export const monthStart = ()=> dayjs().startOf("month").toDate();
+export const now = () => new Date();
+
+export const todayStart = () =>
+    dayjs().startOf("day").toDate();
+
+export const todayEnd = () =>
+    dayjs().endOf("day").toDate();
+
+export const monthStart = () =>
+    dayjs().startOf("month").toDate();
+
+export const monthEnd = () =>
+    dayjs().endOf("month").toDate();
+
+export const subtractDays = (days) =>
+    dayjs().subtract(days, "day").toDate();
+
+export const isSameDay = (date1, date2) =>
+    dayjs(date1).isSame(date2, "day");
